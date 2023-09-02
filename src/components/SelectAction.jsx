5a75907c7af1276d26dff96a8aa6e8f5f01c9560
egyyudanugraha <egyyudanugraha@gmail.com>
2023-09-02 16:51:41 +0700

    Rock, paper, scissors

--- src/components/SelectAction.jsx
+++ src/components/SelectAction.jsx
@@ -3,17 +3,11 @@ import { Card, CardContent } from "./ui/card"
 
 const SelectAction = ({ name, selected, handleSelect }) => {
 
-  const handleChoice = () => {
-    console.log('Clicked');
-    handleSelect(name)
-    console.log('Done');
-  }
-
   return (
     <Card 
       className={`w-40 cursor-pointer ${selected === name && 'border-2 border-slate-900'}`}
       name={name}
-      onClick={handleChoice}
+      onClick={() => handleSelect(name)}
     >
       <CardContent className="flex justify-center items-center p-6">
         <Image
